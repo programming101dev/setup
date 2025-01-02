be267cd54649d61cf76dbb40facda4bd19ad9441
programming101dev <113087924+programming101dev@users.noreply.github.com>
2025-01-02 13:21:44 -0800

    remove unneeded items

--- setup-macos.sh
+++ setup-macos.sh
@@ -12,7 +12,6 @@ brew_packages=(
     gcc
     gpg
     graphviz
-    llvm
     pari
     tmux
     wget
@@ -21,26 +20,9 @@ brew_packages=(
 # Install packages with Homebrew
 for package in "${brew_packages[@]}"; do
     echo "Installing $package with Homebrew..."
-    if [[ "$package" == "wireshark" || "$package" == "obs" || "$package" == "kdenlive" ]]; then
-        brew install --cask "$package" || handle_error "Failed to install $package with Homebrew."
-    else
-        brew install "$package" || handle_error "Failed to install $package with Homebrew."
-    fi
-
+    brew install "$package" || handle_error "Failed to install $package with Homebrew."
 done
 
-# Additional setup for llvm
-llvm_path=$(brew --prefix llvm)/bin
-if ! grep -q "$llvm_path" ~/.zshrc; then
-    echo "Adding LLVM to PATH in ~/.zshrc..."
-    echo "export PATH=\"$llvm_path:\$PATH\"" >> ~/.zshrc || handle_error "Failed to update PATH for LLVM."
-fi
-
-# Install hping3 with MacPorts
-sudo port install hping3 || handle_error "Failed to install hping3 with MacPorts."
-
-# Additional setup for /etc/zshenv
-sudo bash -c 'echo "export PATH=\"$PATH:/usr/local/opt/gcc/bin:/Applications/CMake.app/Contents/bin\"" >> /etc/zshenv' || handle_error "Failed to update /etc/zshenv."
 sudo bash -c 'echo "export MallocNanoZone=0" >> /etc/zshenv' || handle_error "Failed to add MallocNanoZone to /etc/zshenv."
 
 # Completion message
