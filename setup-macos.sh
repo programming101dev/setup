8969eb748ee849c9972e04513cd81aa25ca1e5a2
programming101dev <113087924+programming101dev@users.noreply.github.com>
2025-01-01 20:08:26 -0800

    cleanup what gets installed

--- setup-macos.sh
+++ setup-macos.sh
@@ -6,43 +6,17 @@ handle_error() {
     exit 1
 }
 
-# Install Xcode command line tools
-if ! xcode-select -p > /dev/null 2>&1; then
-    echo "Installing Xcode command line tools..."
-    xcode-select --install || handle_error "Failed to install Xcode command line tools."
-fi
-
-# Agree to Xcode license
-sudo xcodebuild -license accept || handle_error "Failed to accept Xcode license."
-
-# Install Homebrew
-if ! command -v brew > /dev/null 2>&1; then
-    echo "Installing Homebrew..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" || handle_error "Failed to install Homebrew."
-fi
-
-# Install MacPorts
-if ! command -v port > /dev/null 2>&1; then
-    echo "Installing MacPorts..."
-    curl -O https://distfiles.macports.org/MacPorts/MacPorts-2.8.1.tar.bz2 || handle_error "Failed to download MacPorts."
-    tar xf MacPorts-2.8.1.tar.bz2
-    cd MacPorts-2.8.1 && ./configure && make && sudo make install || handle_error "Failed to install MacPorts."
-    cd .. && rm -rf MacPorts-2.8.1*
-fi
-
 # List of packages to install with Homebrew
 brew_packages=(
-    llvm
-    gcc
-    cppcheck
-    pari
-    wget
-    graphviz
-    tmux
     cmake
-    obs
-    kdenlive
-    wireshark
+    cppcheck
+    gcc
+    gpg
+    graphviz
+    llvm
+    pari
+    tmux
+    wget
 )
 
 # Install packages with Homebrew
@@ -69,14 +43,6 @@ sudo port install hping3 || handle_error "Failed to install hping3 with MacPorts
 # Additional setup for /etc/zshenv
 sudo bash -c 'echo "export PATH=\"$PATH:/usr/local/opt/gcc/bin:/Applications/CMake.app/Contents/bin\"" >> /etc/zshenv' || handle_error "Failed to update /etc/zshenv."
 sudo bash -c 'echo "export MallocNanoZone=0" >> /etc/zshenv' || handle_error "Failed to add MallocNanoZone to /etc/zshenv."
-
-# Configure Wireshark
-if [ -d "/Applications/Wireshark.app" ]; then
-    echo "Configuring Wireshark..."
-    sudo installer -pkg "/Applications/Wireshark.app/Contents/Resources/ChmodBPF.pkg" -target / || handle_error "Failed to install ChmodBPF."
-else
-    echo "Wireshark not installed. Skipping configuration."
-fi
 
 # Completion message
 echo "All tools installed and configured successfully. Please reboot your computer for changes to take effect."
