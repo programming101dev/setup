de28d127e560bae5f4d0953e20529a06cd1fa452
programming101dev <113087924+programming101dev@users.noreply.github.com>
2025-02-11 10:37:32 -0800

    fix for sh

--- setup-freebsd.sh
+++ setup-freebsd.sh
@@ -10,27 +10,11 @@ handle_error() {
 pkg update || handle_error "Failed to update package lists."
 pkg upgrade -y || handle_error "Failed to upgrade packages."
 
-# List of packages to install with pkg
-pkg_packages=(
-    bash
-    wget
-    nano
-    hping3
-    git
-    cmake
-    gcc
-    llvm
-    cppcheck
-    pari
-    python3
-    lsof
-    gnugp
-    graphviz
-    sudo
-)
+# List of packages (POSIX-compatible format)
+pkg_packages="bash wget nano hping3 git cmake gcc llvm cppcheck pari python3 lsof gnupg graphviz sudo"
 
 # Install packages with pkg
-for package in "${pkg_packages[@]}"; do
+for package in $pkg_packages; do
     echo "Installing $package with pkg..."
     pkg install -y "$package" || handle_error "Failed to install $package with pkg."
 done
@@ -54,6 +38,7 @@ sysrc -f /etc/rc.conf ldconfig_paths="/usr/local/lib /usr/local/lib64"
 # Reload ldconfig paths
 ldconfig -m /usr/local/lib /usr/local/lib64
 
+# Grant sudo access to all users (with password)
 echo "ALL ALL=(ALL) ALL" | sudo tee -a /usr/local/etc/sudoers.d/all-users
 
 ./setup-groups.sh
